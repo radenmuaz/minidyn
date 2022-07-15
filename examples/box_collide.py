bdeb7aa21f30489d29c5be20dd8de43784f12d3d
Muaz Raden <muazraden@Muazs-MacBook-Air.local>
2022-07-15 22:56:19 +0800

    SAT

--- examples/box_collide.py
+++ examples/box_collide.py
@@ -8,7 +8,7 @@ import trimesh
       
 if __name__ == "__main__":
     world = dyn.World()
-    world.add_ground()
+    # world.add_ground()
 
     box_body = dyn.Body()
     box_mass = 1.
@@ -19,18 +19,18 @@ if __name__ == "__main__":
                                         )
     box_shape = trimesh.creation.box((1., 1., 1.))
     box_body.shapes = [mdn.col.Shape.from_trimesh(box_shape)]
-    world.add_body(box_body, q=jnp.array([1., 0., 0, 0., 1, 0. , 2.]))
+    world.add_body(box_body, q=jnp.array([1., 0., 0, 0., 1, 0. , 1.]))
 
-    box_body2 = dyn.Body()
-    box_mass2 = 1.
-    box_moment2 = jnp.eye(3) * box_mass
-    box_body2.inertia = dyn.Inertia(mass=box_mass2,
-                        moment=box_moment2,
-                        com=jnp.array((0, 0, 0)),
-                                        )
-    box_shape2 = trimesh.creation.box((2., 2., 2.))
-    box_body2.shapes = [mdn.col.Shape.from_trimesh(box_shape2)]
-    world.add_body(box_body2, q=jnp.array([1., 0., 0, 0., -1., 0. , 2.]))
+    # box_body2 = dyn.Body()
+    # box_mass2 = 2.
+    # box_moment2 = jnp.eye(3) * box_mass
+    # box_body2.inertia = dyn.Inertia(mass=box_mass2,
+    #                     moment=box_moment2,
+    #                     com=jnp.array((0, 0, 0)),
+    #                                     )
+    # box_shape2 = trimesh.creation.box((2., 2., 2.))
+    # box_body2.shapes = [mdn.col.Shape.from_trimesh(box_shape2)]
+    # world.add_body(box_body2, q=jnp.array([1., 0., 0, 0., 0., 0. , 1.]))
 
     world_solver = dyn.WorldSolver()
     
